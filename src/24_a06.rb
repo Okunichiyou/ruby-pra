8983884f67b79c5a3df05805fa644be798160009
奥日曜 <seifuzi@icloud.com>
2026-01-16 09:26:21 +0900

    リファクタリング
    ・変数名の改良
    ・amount_days[end_day] - amount_days[start_day]) + (amount_days[start_day] - amount_days[start_day - 1]
    はamount_days[end_day] -  amount_days[start_day - 1]と言えるので計算式を簡略化

--- src/24_a06.rb
+++ src/24_a06.rb
@@ -1,22 +1,32 @@
-n, q = gets.chomp.split.map(&:to_i)
-attendees = gets.chomp.split.map(&:to_i)
+# 日数と参加者の入出力処理
+days_count, queries_count = gets.chomp.split.map(&:to_i)
+daily_participants = gets.chomp.split.map(&:to_i)
 
-questions = []
-q.times do
-  l, r = gets.chomp.split.map(&:to_i)
-  questions << { start: l, end: r }
+# クエリ（範囲指定）を読み込み
+queries = []
+queries_count.times do
+  left, right = gets.chomp.split.map(&:to_i)
+  queries << { start: left, end: right }
 end
 
-amount_days = [0]
-
-n.times do |i|
-  sum = amount_days[i] + attendees[i]
-  amount_days << sum
+# 累積和を計算（0から開始）
+cumulative_sum = [ 0 ]
+days_count.times do |day_index|
+  total = cumulative_sum[day_index] + daily_participants[day_index]
+  cumulative_sum << total
 end
 
-questions.each do |question|
-  puts (amount_days[question[:end]] - amount_days[question[:start]]) + (amount_days[question[:start]] - amount_days[question[:start] - 1])
+# 各クエリに対して範囲の参加者数を計算
+queries.each do |query|
+  start_day = query[:start]
+  end_day = query[:end]
+
+  # 範囲内の参加者数を計算
+  total_in_range = cumulative_sum[end_day] - cumulative_sum[start_day - 1]
+
+  puts total_in_range
 end
+
 
 # 元々書いてた以下のコードはパフォーマンスが非常に悪くてタイムアウトになっていた
 # _n, q = gets.chomp.split.map(&:to_i)
