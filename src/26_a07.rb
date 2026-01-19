195557bc153bb8d250c4311040c28a54fe33d729
Okunichiyou <seifuzi@icloud.com>
2026-01-19 23:56:33 +0900

    もとす法で2 * 10 ** 5に

--- src/26_a07.rb
+++ src/26_a07.rb
@@ -1,17 +1,18 @@
 d = gets.to_i
 n = gets.to_i
 
-results = [0] * d
+results = [0] * (d + 1)
 
-# 最大 10 ** 5
-n.times do |i|
+# 10 ** 5
+n.times do
   l, r = gets.chomp.split.map(&:to_i)
-  range = []
-  # 最大 10 ** 5
-  results[(l - 1)..(r - 1)] = results[(l - 1)..(r - 1)].map { it + 1}
+  results[l-1] += 1
+  results[r] -= 1
 end
 
-# 最大 10 ** 5
-results.each do |count|
+count = 0
+# 10 ** 5
+d.times do |i|
+  count += results[i]
   puts count
 end
