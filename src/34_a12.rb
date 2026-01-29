230db3bc92ab9443627b1eecf4d8370649fbb550
Okunichiyou <seifuzi@icloud.com>
2026-01-29 22:44:48 +0900

    a12: 2分探索で計算

--- src/34_a12.rb
+++ src/34_a12.rb
@@ -1,5 +1,23 @@
 n, k = gets.split.map(&:to_i)
+a_arr = gets.split.map(&:to_i)
 
-a_arr = gets.chomp.split.map { Rational(1, it.to_i) }
+# 時間tで印刷できる枚数を計算
+def count_prints(a_arr, t)
+  a_arr.sum { |a| t / a }
+end
 
-puts (k / a_arr.sum).ceil
+# 二分探索
+left = 0
+right = a_arr.min * k
+
+# 最大 30回 = log (10 ** 9)
+while right - left > 1
+  mid = (left + right) / 2
+  if count_prints(a_arr, mid) >= k
+    right = mid
+  else
+    left = mid
+  end
+end
+
+puts right
