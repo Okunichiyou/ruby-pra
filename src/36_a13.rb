858577eea356e83665b6e51d1ce7d01c6a4180bc
Okunichiyou <seifuzi@icloud.com>
2026-02-02 08:56:35 +0900

    a13: 2部探索に変更

--- src/36_a13.rb
+++ src/36_a13.rb
@@ -2,8 +2,25 @@ n, k = gets.split.map(&:to_i)
 
 arrays = gets.chomp.split.map(&:to_i)
 
-filtered = arrays.combination(2).select do |arr|
-  arr[1] - arr[0] <= k
+result = 0
+# 10万回探索 = n - 1
+n.times do |i|
+  left_pair = arrays[i]
+  max_right_pair = left_pair + k
+  # log2(n)探索
+  left = i
+  right = n
+
+  while left < right
+    mid = (left + right) / 2
+    if arrays[mid] > max_right_pair
+      right = mid
+    else
+      left = mid + 1
+    end
+  end
+
+  result += (left - i - 1)
 end
 
-puts filtered.size
+puts result
